8fea30cdf0a68cc67944b4b68e69a928e078627a
taewonkim <kimtae9217@gmail.com>
2019-11-24 18:05:19 +0900

    dkafjsldkfj

---
+++ LotteryNumbers/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace LotteryNumbers
+{
+    partial class Lottery
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Lottery));
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtDisplay1 = new System.Windows.Forms.TextBox();
+            this.txtDisplay2 = new System.Windows.Forms.TextBox();
+            this.txtDisplay3 = new System.Windows.Forms.TextBox();
+            this.txtDisplay4 = new System.Windows.Forms.TextBox();
+            this.txtDisplay5 = new System.Windows.Forms.TextBox();
+            this.txtDisplay6 = new System.Windows.Forms.TextBox();
+            this.txtGoodLuck = new System.Windows.Forms.TextBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.button1.Location = new System.Drawing.Point(137, 368);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtDisplay1
+            // 
+            this.txtDisplay1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDisplay1.Location = new System.Drawing.Point(38, 295);
+            this.txtDisplay1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtDisplay1.Name = "txtDisplay1";
+            this.txtDisplay1.Size = new System.Drawing.Size(39, 30);
+            this.txtDisplay1.TabIndex = 1;
+            // 
+            // txtDisplay2
+            // 
+            this.txtDisplay2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDisplay2.Location = new System.Drawing.Point(101, 295);
+            this.txtDisplay2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtDisplay2.Name = "txtDisplay2";
+            this.txtDisplay2.Size = new System.Drawing.Size(52, 30);
+            this.txtDisplay2.TabIndex = 2;
+            // 
+            // txtDisplay3
+            // 
+            this.txtDisplay3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDisplay3.Location = new System.Drawing.Point(164, 295);
+            this.txtDisplay3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtDisplay3.Name = "txtDisplay3";
+            this.txtDisplay3.Size = new System.Drawing.Size(52, 30);
+            this.txtDisplay3.TabIndex = 3;
+            // 
+            // txtDisplay4
+            // 
+            this.txtDisplay4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDisplay4.Location = new System.Drawing.Point(225, 295);
+            this.txtDisplay4.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtDisplay4.Name = "txtDisplay4";
+            this.txtDisplay4.Size = new System.Drawing.Size(52, 30);
+            this.txtDisplay4.TabIndex = 4;
+            // 
+            // txtDisplay5
+            // 
+            this.txtDisplay5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDisplay5.Location = new System.Drawing.Point(287, 295);
+            this.txtDisplay5.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtDisplay5.Name = "txtDisplay5";
+            this.txtDisplay5.Size = new System.Drawing.Size(52, 30);
+            this.txtDisplay5.TabIndex = 5;
+            // 
+            // txtDisplay6
+            // 
+            this.txtDisplay6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDisplay6.Location = new System.Drawing.Point(348, 295);
+            this.txtDisplay6.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtDisplay6.Name = "txtDisplay6";
+            this.txtDisplay6.Size = new System.Drawing.Size(52, 30);
+            this.txtDisplay6.TabIndex = 6;
+            // 
+            // txtGoodLuck
+            // 
+            this.txtGoodLuck.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.txtGoodLuck.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtGoodLuck.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGoodLuck.Location = new System.Drawing.Point(128, 332);
+            this.txtGoodLuck.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtGoodLuck.Name = "txtGoodLuck";
+            this.txtGoodLuck.Size = new System.Drawing.Size(173, 30);
+            this.txtGoodLuck.TabIndex = 8;
+            this.txtGoodLuck.Text = "축하합니다!";
+            this.txtGoodLuck.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtGoodLuck.Visible = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "KakaoTalk_20191122_160729004.png");
+            this.imageList1.Images.SetKeyName(1, "KakaoTalk_20191122_160729004_01.png");
+            this.imageList1.Images.SetKeyName(2, "KakaoTalk_20191122_160729004_02.png");
+            this.imageList1.Images.SetKeyName(3, "KakaoTalk_20191122_160729004_03.png");
+            this.imageList1.Images.SetKeyName(4, "KakaoTalk_20191122_160729004_04.png");
+            this.imageList1.Images.SetKeyName(5, "KakaoTalk_20191122_162835614.png");
+            this.imageList1.Images.SetKeyName(6, "KakaoTalk_20191122_162835614_01.png");
+            this.imageList1.Images.SetKeyName(7, "KakaoTalk_20191122_162835614_02.png");
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.label1.Location = new System.Drawing.Point(58, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(327, 267);
+            this.label1.TabIndex = 9;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(23, 279);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(71, 62);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Lottery
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.ClientSize = new System.Drawing.Size(436, 405);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtGoodLuck);
+            this.Controls.Add(this.txtDisplay6);
+            this.Controls.Add(this.txtDisplay5);
+            this.Controls.Add(this.txtDisplay4);
+            this.Controls.Add(this.txtDisplay3);
+            this.Controls.Add(this.txtDisplay2);
+            this.Controls.Add(this.txtDisplay1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.Coral;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Lottery";
+            this.Text = "Lottery Numbers Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtDisplay1;
+        private System.Windows.Forms.TextBox txtDisplay2;
+        private System.Windows.Forms.TextBox txtDisplay3;
+        private System.Windows.Forms.TextBox txtDisplay4;
+        private System.Windows.Forms.TextBox txtDisplay5;
+        private System.Windows.Forms.TextBox txtDisplay6;
+        private System.Windows.Forms.TextBox txtGoodLuck;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ LotteryNumbers/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace LotteryNumbers
+{
+    public partial class Lottery : Form
+    {
+        public Lottery()
+        {
+            InitializeComponent();
+        }
+        // Setup array as integers 
+        int[] myNumbers;
+
+        private static DateTime Delay(int MS)
+        {
+            DateTime ThisMoment = DateTime.Now;
+            TimeSpan duration = new TimeSpan(0, 0, 0, 0, MS);
+            DateTime AfterWards = ThisMoment.Add(duration);
+
+            while (AfterWards >= ThisMoment)
+            {
+                System.Windows.Forms.Application.DoEvents();
+                ThisMoment = DateTime.Now;
+            }
+
+            return DateTime.Now;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //set myNumbers unvisible
+            txtDisplay1.Text = "";
+            txtDisplay2.Text = "";
+            txtDisplay3.Text = "";
+            txtDisplay4.Text = "";
+            txtDisplay5.Text = "";
+            txtDisplay6.Text = "";
+            Delay(1000);
+            // set rnd as random variable
+            Random rnd = new Random();
+            // Set array length
+            myNumbers = new int[6];
+            //init tempNumber
+            int tmpNumber = 0;
+            // For loop to iterate through array 
+            for (int i = 0; i < 6; i++)
+            {
+                // Generate random number
+                tmpNumber = rnd.Next(1, 45);
+                
+                //Check tmpNumber is not in myNumbers
+                if (!myNumbers.Contains(tmpNumber))
+                {
+                    //Add number to array
+                    myNumbers[i] = tmpNumber;
+                }
+                else
+                {
+                    //redo
+                    i--;
+                }
+            }
+            // Sort array, lowest number first
+            //Array.Sort(myNumbers);
+
+            // Give each textbox the value stored in array[index]
+            
+            txtDisplay1.Text = myNumbers[0].ToString();
+            Delay(2000);
+            txtDisplay2.Text = myNumbers[1].ToString();
+            Delay(2000);
+            txtDisplay3.Text = myNumbers[2].ToString();
+            Delay(2000);
+            txtDisplay4.Text = myNumbers[3].ToString();
+            Delay(2000);
+            txtDisplay5.Text = myNumbers[4].ToString();
+            Delay(2000);
+            txtDisplay6.Text = myNumbers[5].ToString();
+            Delay(2000);
+            
+            txtGoodLuck.Visible = true;
+        
+            
+
+    }
+        private int index = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            index %= imageList1.Images.Count;
+            label1.Image = imageList1.Images[index++];
+        }
+    }
+}
